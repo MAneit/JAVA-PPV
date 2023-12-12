9b3f5324752b97d7a70050851dbd012162a3ec36
maneit <149658378+MAneit@users.noreply.github.com>
2023-12-12 09:23:59 +0200

    Add files via upload

---
+++ userregistration/src/main/java/www/userregistration/Userregistration.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package www.userregistration;
+
+/**
+ *
+ * @author USER
+ */
+public class Userregistration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
